4ee4718d62465bdc8eccd44047fe44ec67ce2448
MarniaTu <147163020+MarniaTu@users.noreply.github.com>
2023-11-04 17:07:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 15_849;
+        int rubPerMile = 20;
+        int miles = amount / rubPerMile;
+
+        System.out.println("Вам начислено " + miles + " миль(и)");
+
+    }
+}
